4738ea2e4060c5a657e9cd8f3411c97153183640
joshsanyal <josh.sanyal@yahoo.com>
2024-02-09 19:13:15 -0800

    Categories get session_ids from embed similarity

--- myapi/categories.py
+++ myapi/categories.py
@@ -1,47 +1,54 @@
 # User-defined categories
 from .callgpt import *
 from .consts import *
+import numpy as np
+from sklearn.metrics.pairwise import cosine_similarity
 
-# Stores the user-defined categories
-categories = [{"name": "NAME", "description": "DESCRIPTION"}]
+# Creates embeddings for all llm events
+def embed_llm_events():
+    # Grab the llm events
+    llm_df = df[df['event_type'] == 'llm']
+    llm_df['event_text'] = 'Event name: ' + llm_df['event_name'] + \
+                        '\n Input: ' + llm_df['input_content'] + \
+                        '\n Output: ' + llm_df['output_content']
 
-# Define the categories dataframe, columns are sessions_id and category booleans
-categories_df = pd.DataFrame()
+    embeds = np.array(embeddings_model.embed_documents(llm_df['event_text'].to_list()))
+    llm_df['embeds'] = [np.array(e) for e in embeds]
+    return llm_df
+
+# Store the embeddings for all llm_events
+llm_df = embed_llm_events()
+
+# Stores the user-defined categories and distinct event ids that correspond to it
+categories = [{"name": "NAME", "description": "DESCRIPTION", "session_ids": []}]
 
 # Add a new category
 def add_category(name, description):
-    categories.append({"name": name, "description": description})
-    print("add")
-    print(categories)
-    
+    new_category = {"name": name, "description": description, "session_ids": []}
+    new_category = assign_session_ids_to_category(new_category)
+    categories.append(new_category)
+
 # Get all categories
 def get_categories():
     return categories
 
 # function to delete a category from the list
 def delete_category(index):
-    # when displaying categories index is reversed
-    print("ran")
-    print(categories)
-    categories.pop(len(categories) - int(index) - 1)
-    print(categories)
+    categories.pop(len(categories) - int(index) - 1) # when displaying categories index is reversed
 
-# TODO: figure out how to assign categories
-def assign_category(row, category_description):
-    event_id = row["event_id"]
-    event_name = df["event_name"][event_id]
-    input = df["input_content"][event_id]
-    output = df["output_content"][event_id]
+# Embed the category description and add all session ids
+def assign_session_ids_to_category(category, similarity_threshold=0.66):
+    # Embed the category description
+    category_embedding = np.array(embeddings_model.embed_documents([category["description"]]))[0]
+    category_embedding = category_embedding.reshape(1, -1)
 
-    response_obj = query_gpt(client, build_classify_event_prompt(category_description, event_name, input, output),
-                             model="gpt-3.5-turbo", max_tokens=10)
-    return response_obj == "True"
+    # Find similarity between the category description and the llm events
+    llm_df[category['name']] = cosine_similarity(category_embedding, list(llm_df['embeds'])).flatten()
 
-def create_categories_df():
-    categories_df["event_id"] = df[df["event_type"] == 'llm'].index
-    # categories_df[categories[0]["name"]] = categories_df.apply(assign_category, args=(categories[0]["description"],), axis=1)
-
-create_categories_df()   
+    # Find all session ids that have a similarity above the threshold
+    unique_session_ids = llm_df[llm_df[category['name']] > similarity_threshold]['session_id'].unique()
+    category["session_ids"] = unique_session_ids.to_list()
+    return category
 
 def get_session_ids_given_filters(topics, kpis, users):
     sql_query = f"""
@@ -53,7 +60,4 @@ def get_session_ids_given_filters(topics, kpis, users):
     df = pd.DataFrame(data = result.result_rows, columns = result.column_names).sort_values(
         by = ["timestamp"]
     )
-
-
-
 
