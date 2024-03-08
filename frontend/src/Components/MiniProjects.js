55c6cb1e1b943537c76cd949f5f9374ab7338237
avigyabb <avigyabb@stanford.edu>
2024-03-08 00:43:48 -0800

    csv to email

--- frontend/src/Components/MiniProjects.js
+++ frontend/src/Components/MiniProjects.js
@@ -1,90 +1,20 @@
-import Navbar from "./Navbar";
-import { Link } from 'react-router-dom';
 import '../styles/Navbar.css'
 import FileUpload from "./Website/FileUpload";
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
 import '../styles/MiniProjects.css';
+import { Link } from 'react-router-dom';
 
 const MiniProjects = () => {
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const formData = new FormData(event.currentTarget);
-    console.log({
-        email: formData.get('email'),
-        firstName: formData.get('firstName'),
-        lastName: formData.get('lastName'),
-        company: formData.get('company'),
-        role: formData.get('role'),
-        additionalDetails: formData.get('additionalDetails'),
-    });
-    // Here, you would typically send the formData to a server
-  };
 
   return (
-    <>
-      <div className="navbarWrapper flex flex-row mb-5"> 
-        <Link to={`/`} className="ml-5 mr-2 font-bold text-2xl"> rova </Link>
-        <p style={{ fontSize: '12px', color: 'gray'}}> CONTACT US </p>
-        <Link to={``} className="link ml-auto mr-5"> </Link> 
+    <div className="flex h-screen">
+      <div className="left-content pt-5 pl-10">
+        <Link to={`/`} className="font-bold text-4xl text-gray-100"> rova </Link> 
+        <h1 className='text-6xl text-gray-500 font-bold' style={{width: '80%', marginLeft: '5%', marginTop: '25%', lineHeight: '1.5'}}> Analyze Your Chat Logs In 24 Hours </h1>
       </div>
       <div className='signup-form'>
-          <Box
-              className='box'
-              component="form"
-              noValidate
-              autoComplete="off"
-              onSubmit={handleSubmit}
-          >
-              <Typography variant="h6" gutterBottom style={{ color: 'black' }}>
-                  Registration Form
-              </Typography>
-              <div className='flex mb-3 justify-between gap-3'>
-                  <TextField className="flex-grow" required id="firstName" name="firstName" label="First Name" variant="outlined"/>
-                  <TextField className="flex-grow" required id="lastName" name="lastName" label="Last Name" variant="outlined"/>
-                  <TextField
-                      className="flex-grow"
-                      required
-                      id="email"
-                      name="email"
-                      label="Email"
-                      type="email"
-                      variant="outlined"
-                  />
-              </div>
-              <div className='flex mb-3 gap-3'>
-                  <TextField
-                      className="flex-grow"
-                      id="company"
-                      name="company"
-                      label="Company"
-                      variant="outlined"
-                  />
-                  <TextField
-                      id="role"
-                      name="role"
-                      label="Role"
-                      variant="outlined"
-                      className='flex-grow text-field'
-                  />
-              </div>
-              <TextField
-                  id="additionalDetails"
-                  name="additionalDetails"
-                  label="Additional Details"
-                  multiline
-                  rows={4}
-                  variant="outlined"
-              />
-              <Button type="submit" variant="contained" sx={{ mt: 2 }}>
-                  Submit
-              </Button>
-          </Box>
           <FileUpload />
       </div>
-    </>
+    </div>
   )
 }
 
--- frontend/src/Components/Website/FileUpload.js
+++ frontend/src/Components/Website/FileUpload.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 
 const FileUpload = () => {
   const [selectedFiles, setSelectedFiles] = useState([]);
@@ -21,8 +24,6 @@ const FileUpload = () => {
     Array.from(selectedFiles).forEach(file => {
       formData.append('file', file);
     });
-    formData.append('uploader_name', uploaderName);
-    formData.append('uploader_email', uploaderEmail);
 
     try {
       const response = await axios.post(`${process.env.REACT_APP_API_URL}/fileupload/upload/`, formData, {
@@ -38,23 +39,93 @@ const FileUpload = () => {
     }
   };
 
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+
+    console.log({
+        email: formData.get('email'),
+        firstName: formData.get('firstName'),
+        lastName: formData.get('lastName'),
+        company: formData.get('company'),
+        role: formData.get('role'),
+        additionalDetails: formData.get('additionalDetails'),
+        files: formData.get('files')
+    });
+
+    try {
+      const response = await axios.post(`${process.env.REACT_APP_API_URL}fileupload/upload/`, formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+
+      alert(response.data.message);
+    } catch (error) {
+      console.error('Error uploading files:', error);
+      alert('Error uploading files');
+    }
+  };
+
   return (
     <div className="mt-5 flex flex-col" style={{color: "black"}}>
-      {/* <input style={{color: "black"}}
-        type="text"
-        placeholder="Your Name"
-        value={uploaderName}
-        onChange={(e) => setUploaderName(e.target.value)}
-      />
-      <input style={{color: "black"}}
-        type="email"
-        placeholder="Your Email"
-        value={uploaderEmail}
-        onChange={(e) => setUploaderEmail(e.target.value)}
-      /> */}
-      <h1 className='mb-3 text-gray-500'>Get a free report on your data. We typically respond in 24 hours.</h1>
-      <input className="mb-3" type="file" multiple onChange={handleFileChange} />
-      <Button variant="contained" onClick={handleUpload}>Upload Files</Button>
+      <Box
+          className='box'
+          component="form"
+          noValidate
+          autoComplete="off"
+          onSubmit={handleSubmit}
+      >
+          <Typography variant="h3" gutterBottom style={{ color: 'black' }}>
+              Get In Touch
+          </Typography>
+          <p className='text-gray-500 mb-8'> 
+            Fill out the form below to schedule a call with us. For a limited number of spots 
+            we will analyze your chat logs and generate a report for you.
+          </p>
+          <div className='flex mb-3 justify-between gap-3'>
+              <TextField className="flex-grow" required id="firstName" name="firstName" label="First Name" variant="outlined"/>
+              <TextField className="flex-grow" required id="lastName" name="lastName" label="Last Name" variant="outlined"/>
+              <TextField
+                  className="flex-grow"
+                  required
+                  id="email"
+                  name="email"
+                  label="Email"
+                  type="email"
+                  variant="outlined"
+              />
+          </div>
+          <div className='flex mb-3 gap-3'>
+              <TextField
+                  className="flex-grow"
+                  id="company"
+                  name="company"
+                  label="Company"
+                  variant="outlined"
+              />
+              <TextField
+                  id="role"
+                  name="role"
+                  label="Role"
+                  variant="outlined"
+                  className='flex-grow text-field'
+              />
+          </div>
+          <TextField
+              id="additionalDetails"
+              name="additionalDetails"
+              label="Additional Details"
+              multiline
+              rows={4}
+              variant="outlined"
+          />
+          <div className='upload-data mt-5 mb-5'>
+            <h1 className='mb-3 text-gray-500 text-sm'> (Optional) Upload a csv for us to look at. We typically respond in 24 hours.</h1>
+            <input name="files" className="text-gray-500" type="file" multiple onChange={handleFileChange} />
+          </div>
+          <button id="submit-btn"type="submit" variant="contained"> Submit </button>
+      </Box>
     </div>
   );
 };
