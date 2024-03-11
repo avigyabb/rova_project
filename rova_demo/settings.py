f81bbe11f4da6509b60eac8e1cd79d60cf535c81
avigyabb <avigyabb@stanford.edu>
2024-03-11 14:42:21 -0700

    got rid of backend for files

--- rova_demo/settings.py
+++ rova_demo/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
     "rest_framework.authtoken",
     "corsheaders",
     "myapi",
-    "keymetrics",  # why do you need to do .apps.MYapiConfig - Avi
+    "keymetrics",
     "accounts",
     "categories",
     "data_sets",
